79df78c2ee4f1742b59641778a16d2e8975a724c
Dzane <dzane@student.21-school.ru>
2023-06-18 17:25:46 +0700

    feat: hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import { App } from 'app/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from 'redux/store'
 
 const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
 root.render(
-  <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
