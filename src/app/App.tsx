b47a6315becf31fdc6c5b78ddca3289e6153aab9
Dzane <dzane@student.21-school.ru>
2023-06-18 17:44:07 +0700

    feat: delete some

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import { AdvantagesStep } from 'pages/RegistrationPage/components/RegistrationSt
 import { UserInformationStep } from 'pages/RegistrationPage/components/RegistrationSteps/UserInformationStep/UserInformationStep'
 import { RegistrationPage } from 'pages/RegistrationPage/RegistrationPage'
 import React from 'react'
-import { HashRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import styles from './App.module.scss'
 
 export const App = () => {
@@ -14,25 +14,20 @@ export const App = () => {
   return (
     <ChakraProvider>
       <div className={container}>
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path={'registration/*'} element={<RegistrationPage />}>
-              <Route
-                path="step_1"
-                element={<UserInformationStep navigate={navigate} />}
-              />
-              <Route
-                path="step_2"
-                element={<AdvantagesStep navigate={navigate} />}
-              />
-              <Route
-                path="step_3"
-                element={<AboutStep navigate={navigate} />}
-              />
-            </Route>
-          </Routes>
-        </HashRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path={'registration/*'} element={<RegistrationPage />}>
+            <Route
+              path="step_1"
+              element={<UserInformationStep navigate={navigate} />}
+            />
+            <Route
+              path="step_2"
+              element={<AdvantagesStep navigate={navigate} />}
+            />
+            <Route path="step_3" element={<AboutStep navigate={navigate} />} />
+          </Route>
+        </Routes>
       </div>
     </ChakraProvider>
   )
